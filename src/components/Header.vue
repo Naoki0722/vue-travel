1dd32ec8beb3ac37b6b6c181d8309fb5f798f81e
Naoki0722 <mattu.nao722@gmail.com>
2020-12-08 00:00:54 +0900

    1207の成果

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-app-bar
+      color="light-green accent-2"
+      elevate-on-scroll
+      height="100px"
+    >
+      <div class="hidden-sm-and-up">
+        <v-menu
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item @click="$router.push('/')">
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/about')">
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/about')">
+              <v-list-item-title>Contact</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/about')">
+              <v-list-item-title>Search</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+      <div class="title">
+        <v-toolbar-title class="text-h3 hidden-sm-and-down">Travel-Review</v-toolbar-title>
+        <v-toolbar-title class="display-1 hidden-sm-and-up">Travel-Review</v-toolbar-title>
+        <p>〜旅の思い出はシェアから〜</p>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn rounded color="light-blue accent-1" class="mx-4 hidden-sm-and-down" @click="$router.push('/')">Home</v-btn>
+      <v-btn rounded color="light-blue accent-1" class="mx-4 hidden-sm-and-down" @click="$router.push('/about')">About</v-btn>
+      <v-btn rounded color="light-blue accent-1" class="mx-4 hidden-sm-and-down">Contact</v-btn>
+      <v-btn rounded color="light-blue accent-1" class="ml-4 mr-10 hidden-sm-and-down">Search</v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.title p {
+  font-size: 1rem;
+  margin: 0;
+}
+.title {
+  text-align: center;
+  margin-left: 10%;
+}
+
+.title v-toolbar-title {
+  font-size: 1.6rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>見たい観光地の県を表示</p>
+    <div class="chartdiv"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import * as am4core from "@amcharts/amcharts4/core"
+import * as am4maps from "@amcharts/amcharts4/maps"
+// 日本地図を表示
+import am4geodata_japanlow from "@amcharts/amcharts4-geodata/japanLow"
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  mounted() {
+    let map = am4core.create("chartdiv", am4maps.MapChart)
+    map.geodata = am4geodata_japanlow
+    map.projection = new am4maps.projections.Miller()
+    var polygonSeries = map.series.push(new am4maps.MapPolygonSeries())
+    polygonSeries.useGeodata = true
+    polygonSeries.include = [
+      "JP-40",
+      "JP-41",
+      "JP-42",
+      "JP-43",
+      "JP-44",
+      "JP-45",
+      "JP-46",
+    ]
+    // カーソルが乗ったら件名を表示
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    console.log(polygonTemplate.tooltipText)
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = am4core.color("#74B266");
+    let hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = am4core.color("#367B25");
+    console.log(polygonTemplate.tooltipText)
+    
+    // クリックしたら拡大メソッド
+    polygonSeries.mapPolygons.template.events.on("hit", function(ev) {
+      if(ev.target.dataItem.dataContext.name === "Oita") {
+        console.log("大分県です")
+        // this.$router.push('/about')
+      } else {
+        console.log("大分県ではありません")
+      }
+    })
+  },
+  beforeDestroy() {
+    if(this.map) {
+      // オブジェクト削除
+      this.map.dispose()
+    }
+  },
+
 }
 </script>
+
+
+<style scoped>
+.chartdiv {
+  height: 90vh;
+  width: 70vw;
+  margin: 0 auto;
+}
+</style>
