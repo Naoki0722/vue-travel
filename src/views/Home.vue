424b4ab9588e1c142f66c56d0d8ee6c2c94b44ea
Naoki0722 <mattu.nao722@gmail.com>
2020-12-08 07:44:05 +0900

    1208朝の成果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="home">
-    <p>見たい観光地の県を表示</p>
-    <div class="chartdiv"></div>
+    <div class="main_contents">
+      <p>見たい観光地の県を表示</p>
+      <div class="chartdiv"></div>
+    </div>
+    <v-divider
+      class="ma-4"
+      vertical
+    ></v-divider>
+    <div class="sub_contents">
+      <v-card class="mx-auto">
+        <v-img
+          src="https://lh3.googleusercontent.com/proxy/ut2MTHOiN4NpsTPPrfiaB0e8jWqFlK7pO0i_MXsNmQBM5cmbPMfZTyNIKCHZuMKZK_I1eiH5ZT-URDWDx1yeWJ4hIrePYVxOVrQaTIv_tiEF28sAEDM9kg5SBph-Fjxzeffc"
+          alt=""
+          class="align-end"
+        >
+          <v-card-title>太宰府天満宮</v-card-title>
+        </v-img>
+        <v-card-subtitle>福岡県太宰府市</v-card-subtitle>
+        <v-card-text>３号線沿いにある人気観光地</v-card-text>
+        <v-card-actions>
+          <v-btn>詳しくみる</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -13,6 +35,7 @@ import am4geodata_japanlow from "@amcharts/amcharts4-geodata/japanLow"
 export default {
   name: 'Home',
   mounted() {
+    
     let map = am4core.create("chartdiv", am4maps.MapChart)
     map.geodata = am4geodata_japanlow
     map.projection = new am4maps.projections.Miller()
@@ -27,24 +50,30 @@ export default {
       "JP-45",
       "JP-46",
     ]
-    // カーソルが乗ったら件名を表示
+    // カーソルが乗ったら県名を表示
     var polygonTemplate = polygonSeries.mapPolygons.template;
-    console.log(polygonTemplate.tooltipText)
     polygonTemplate.tooltipText = "{name}";
     polygonTemplate.fill = am4core.color("#74B266");
     let hs = polygonTemplate.states.create("hover");
     hs.properties.fill = am4core.color("#367B25");
-    console.log(polygonTemplate.tooltipText)
-    
+    // console.log(hs.properties.fill)
+
     // クリックしたら拡大メソッド
     polygonSeries.mapPolygons.template.events.on("hit", function(ev) {
-      if(ev.target.dataItem.dataContext.name === "Oita") {
+      let pref = ev.target.dataItem.dataContext.name
+      if(pref === "Oita") {
         console.log("大分県です")
-        // this.$router.push('/about')
+        window.open(pref);
+        // this.prefecture(pref)
       } else {
         console.log("大分県ではありません")
       }
     })
+  },
+  methods: {
+    // prefecture(a) {
+    //   this.$router.push(a)
+    // }
   },
   beforeDestroy() {
     if(this.map) {
@@ -58,8 +87,12 @@ export default {
 
 
 <style scoped>
+.home {
+  display: flex;
+  width: 100%;
+}
 .chartdiv {
-  height: 90vh;
+  height: 80vh;
   width: 70vw;
   margin: 0 auto;
 }
