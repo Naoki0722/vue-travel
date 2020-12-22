886c0a5db14f7dbbbcb8c97f53cc77810f8b1d8b
Naoki0722 <mattu.nao722@gmail.com>
2020-12-22 22:47:55 +0900

    20201222_3

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -14,42 +14,7 @@
           elevation="5"
         >
           <v-card-title class="d-block text-center display-1">お問い合わせフォーム</v-card-title>
-          <!-- <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-            class="px-4 mt-4"
-            name="contact"
-            @submit.prevent="onSubmit"
-          >
-            <v-text-field
-              v-model="name"
-              name="name"
-              :counter="10"
-              :rules="nameRules"
-              clearable
-              clear-icon="mdi-close-circle"
-              label="名前"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              name="email"
-              label="メールアドレス"
-              clearable
-              clear-icon="mdi-close-circle"
-              required
-            ></v-text-field>
-            <v-textarea
-              v-model="content"
-              name="content"
-              label="お問い合わせ内容"
-              clearable
-              clear-icon="mdi-close-circle"
-              required
-            ></v-textarea>
-            <v-card-actions>
+            <!-- <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn
                   rounded
@@ -57,25 +22,30 @@
                   class="mr-4 my-10"
                   :loading="loading"
                   :disabled="loading"
-                  @click="loader = 'loading'"
+                  @click="submit"
                 >
                   投稿する
                 </v-btn>
-            </v-card-actions>
-          </v-form> -->
+            </v-card-actions> -->
           <v-text-field
             v-model="name"
             label="お名前"
             required
+            clearable
+            clear-icon="mdi-close-circle"
           />
           <v-text-field
             v-model="email"
             label="メールアドレス"
             required
+            clearable
+            clear-icon="mdi-close-circle"
           />
           <v-textarea
             v-model="message"
             label="お問い合わせ内容"
+            clearable
+            clear-icon="mdi-close-circle"
           />
           <v-text-field
             v-model="botfield"
@@ -88,17 +58,13 @@
             rounded
             color="green accent-2"
             class="mr-4 my-10"
+            :loading="loading"
+            :disabled="loading"
             @click="submit"
             >
               送信
             </v-btn>
           </v-card-actions>
-
-          <!-- <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message"></textarea>
-          </form> -->
         </v-card>
       </v-col>
     </v-row>
@@ -110,49 +76,46 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      // loader: null,
-      // loading: false,
+      loading: false,
       name: "",
       email: "",
       message: ""
 
     }
   },
-  // watch: {
-  //   loader () {
-  //     const l = this.loader
-  //     this[l] = !this[l]
+  methods: {
+    async submit () {
+      this.loading = true
+      setTimeout(() => {
+        const params = new URLSearchParams()
+        params.append('form-name', 'contact2')
+        params.append('name', this.name)
+        params.append('email', this.email)
+        params.append('content', this.message)
+
+        axios
+          .post('/', params)
+          .then(() =>{
+            this.loading = false
+            this.$router.push({name: "ContactThanks"})
+          })
+      }, 3000)
+    },
+  },
+  // methods: {
+  //   async submit() {
   //     const params = new URLSearchParams()
-  //     params.append('form-name', 'contact')
+  //     params.append('form-name', 'contact2')
   //     params.append('name', this.name)
   //     params.append('email', this.email)
-  //     params.append('content', this.content)
-  //     console.log(params.getAll('message'))
+  //     params.append('content', this.message)
 
-        
-  //     setTimeout(() => (
   //     axios
   //       .post('/', params)
   //       .then(() => {
   //         this.$router.push({name: "ContactThanks"})
   //       })
-  //     ), 3000)      
-  //   },
+  //   }
   // },
-  methods: {
-    async submit() {
-      const params = new URLSearchParams()
-      params.append('form-name', 'contact2')
-      params.append('name', this.name)
-      params.append('email', this.email)
-      params.append('content', this.message)
-
-      axios
-        .post('/', params)
-        .then(() => {
-          this.$router.push({name: "ContactThanks"})
-        })
-    }
-  },
 }
 </script>
